a2f6d9e5d436d6b4acd7d171867aea786859ba11
Yuriy Meresin <meresin.y@outlook.com>
2025-03-18 14:06:57 +0300

    add red class for dislikes

--- js/likes.js
+++ js/likes.js
@@ -1,6 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
 	var likeButtons = document.querySelectorAll('.like-plus, .like-minus');
 
+	function updateLikeCount(postId, totalCount) {
+		var likeCountElement = document.getElementById('like-count-' + postId);
+		likeCountElement.textContent = totalCount;
+
+		if (totalCount < 0) {
+			likeCountElement.classList.add('red');
+		} else {
+			likeCountElement.classList.remove('red');
+		}
+	}
+
+	likeButtons.forEach(function (button) {
+		var postId = button.getAttribute('data-post-id');
+		var initialLikesCount = parseInt(document.getElementById('like-count-' + postId).textContent) || 0;
+		updateLikeCount(postId, initialLikesCount);
+	});
+
 	likeButtons.forEach(function (button) {
 		button.addEventListener('click', function () {
 			var postId = this.getAttribute('data-post-id');
@@ -20,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function () {
 							var dislikesCount = parseInt(response.data.dislikes) || 0;
 							var totalCount = likesCount - dislikesCount;
 
-							document.getElementById('like-count-' + postId).textContent = totalCount;
+							updateLikeCount(postId, totalCount);
 						} else {
 							console.log('Error in response data:', response.data);
 						}
